419889403b69f1b78f0587041d8956298634cb0e
Andrea Angella <angella.andrea@gmail.com>
2014-03-09 15:30:54 +0000

    Added color image application

---
+++ ColorImage/MainForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ColorImage
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.pbColor = new System.Windows.Forms.PictureBox();
+            this.tbSensitivity = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbColor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSensitivity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.Color = System.Drawing.Color.Red;
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(1441, 46);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(132, 37);
+            this.btnColor.TabIndex = 1;
+            this.btnColor.Text = "Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImage.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pbImage.Image = global::ColorImage.Properties.Resources.image8;
+            this.pbImage.Location = new System.Drawing.Point(12, 12);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(1422, 837);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImage.TabIndex = 4;
+            this.pbImage.TabStop = false;
+            this.pbImage.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbImage_MouseUp);
+            // 
+            // pbColor
+            // 
+            this.pbColor.BackColor = System.Drawing.Color.Red;
+            this.pbColor.Location = new System.Drawing.Point(1441, 12);
+            this.pbColor.Name = "pbColor";
+            this.pbColor.Size = new System.Drawing.Size(131, 28);
+            this.pbColor.TabIndex = 2;
+            this.pbColor.TabStop = false;
+            // 
+            // tbSensitivity
+            // 
+            this.tbSensitivity.LargeChange = 1000000;
+            this.tbSensitivity.Location = new System.Drawing.Point(1442, 107);
+            this.tbSensitivity.Maximum = 8000000;
+            this.tbSensitivity.Name = "tbSensitivity";
+            this.tbSensitivity.Size = new System.Drawing.Size(130, 45);
+            this.tbSensitivity.SmallChange = 100000;
+            this.tbSensitivity.TabIndex = 6;
+            this.tbSensitivity.TickFrequency = 1000000;
+            this.tbSensitivity.Value = 1000000;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1439, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Sensitivity";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSensitivity);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.pbColor);
+            this.Controls.Add(this.btnColor);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Color Image";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbColor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSensitivity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.PictureBox pbColor;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.TrackBar tbSensitivity;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ColorImage/MainForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ColorImage.Properties;
+using ConnectivityProblem;
+
+namespace ColorImage
+{
+    public partial class MainForm : Form
+    {
+        private WeightedQuickUnionPathCompression finder;
+
+        private readonly Bitmap bitmap;
+        private readonly int bitmapWidth;
+        private readonly int bitmapHeight;
+        private readonly int sensitivity;
+
+        const int PixelSize = 4;
+
+        private bool buildCompleted;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            bitmap = (Bitmap)pbImage.Image;
+            bitmapWidth = bitmap.Width;
+            bitmapHeight = bitmap.Height;
+            sensitivity = tbSensitivity.Value;
+
+            Task.Run(() => BuildConnectivity());
+        }
+
+        // More details for low level bitmap manipulation: http://bobpowell.net/lockingbits.aspx
+        private void BuildConnectivity()
+        {
+            finder = new WeightedQuickUnionPathCompression(bitmapWidth * bitmapHeight);
+
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmapWidth, bitmapHeight),
+                                             ImageLockMode.ReadWrite,
+                                             bitmap.PixelFormat);
+
+            unsafe
+            {
+                for (int y = 1; y < bitmapData.Height - 2; y++)
+                {
+                    byte* rowUp = (byte*)bitmapData.Scan0 + ((y - 1) * bitmapData.Stride);
+                    byte* row = (byte*)bitmapData.Scan0 + (y * bitmapData.Stride);
+                    byte* rowDown = (byte*)bitmapData.Scan0 + ((y + 1) * bitmapData.Stride);
+
+                    for (int x = 1; x < bitmapData.Width - 1; x++)
+                    {
+                        int* value = (int*)(row + x * PixelSize);
+                        int* valueUp = (int*)(rowUp + x * PixelSize);
+                        int* valueDown = (int*)(rowDown + x * PixelSize);
+                        int* valueLeft = (int*)(row + (x - 1) * PixelSize);
+                        int* valueRight = (int*)(row + (x + 1) * PixelSize);
+                        
+                        // Connect pixels that have a similar color (based on sensitivity)
+                        if (Math.Abs(*value - *valueUp) < sensitivity)    finder.Connect(x + y * bitmapWidth, x + (y - 1) * bitmapWidth);
+                        if (Math.Abs(*value - *valueDown) < sensitivity)  finder.Connect(x + y * bitmapWidth, x + (y + 1) * bitmapWidth);
+                        if (Math.Abs(*value - *valueLeft) < sensitivity)  finder.Connect(x + y * bitmapWidth, (x - 1) + y * bitmapWidth);
+                        if (Math.Abs(*value - *valueRight) < sensitivity) finder.Connect(x + y * bitmapWidth, (x + 1) + y * bitmapWidth);
+                    }
+                }
+            }
+
+            bitmap.UnlockBits(bitmapData);
+            buildCompleted = true;
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pbColor.BackColor = colorDialog1.Color;
+            }           
+        }
+
+        private void pbImage_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!buildCompleted) return;
+
+            // Convert mouse coordinates to bitmap coordinates
+            int bx = (int)(e.X / (double)pbImage.Width * bitmapWidth);
+            int by = (int)(e.Y / (double)pbImage.Height * bitmapHeight);
+
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmapWidth, bitmapHeight),
+                                             ImageLockMode.WriteOnly,
+                                             bitmap.PixelFormat);
+
+            unsafe
+            {
+                int newColor = pbColor.BackColor.ToArgb();
+
+                finder.ForEachConnected(bx + by * bitmapWidth, p =>
+                {
+                    // Convert from finder point to bitmap coordinates
+                    int x = p % bitmapWidth;
+                    int y = p / bitmapWidth;
+
+                    byte* row = (byte*)bitmapData.Scan0 + (y * bitmapData.Stride);
+                    int* v = (int*)(row + x * PixelSize);
+
+                    // Set the color
+                    *v = newColor;
+                });
+            }
+
+            bitmap.UnlockBits(bitmapData);           
+            pbImage.Refresh();
+        }
+    }
+}
